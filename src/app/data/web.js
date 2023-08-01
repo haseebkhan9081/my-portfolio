584b3b5be8ac3d4ee521f27cba5a84f9d8f23488
haseebkhan9081 <88801305+haseebkhan9081@users.noreply.github.com>
2023-08-01 11:27:28 +0500

    Updated data for a new project

--- src/app/data/web.js
+++ src/app/data/web.js
@@ -5,6 +5,16 @@ import SocketIcon from "../../../public/bolt-solid.svg";
 import JavascriptIcon from '@mui/icons-material/Javascript';
 
 let web=[{
+name:"Messenger-X",
+Description:"Messenger-X is a real-time chat application that excels with its Pusher-powered messaging, Tailwind-designed sleek UI, and seamless Google/Github authentication. Users can share files, customize profiles, and enjoy read receipts, while online status and group/one-on-one messaging enhance the experience. The app's robust server-side routes and real-time interactions empower users to create and manage chat rooms and channels effortlessly.",
+img:"https://raw.githubusercontent.com/haseebkhan9081/messenger-x/3446a5371c564df61f8cbac109a4dfb892ecb7df/2023-08-01%20(10).png",
+link:"https://messenger-x.vercel.app/",
+TechUsed:[{name:"React",icon:<ReactIcon/>,color:"#3998b6",bgcolor:'#3998b633',},
+{name:"Node",icon:<NodeIcon/>,color:"#43853D",bgcolor:'#54a24533'},
+{name:"Socket.io",icon:<SocketIcon/>,color:"#FFFFFF",bgcolor:'#11182766'},
+ ],
+GithubLink:"https://github.com/haseebkhan9081/messenger-x.git",
+},{
 name:"Global Keeper App",
 Description:"THE MERN WEBAPP IS A FULL-STACK APPLICATION BUILT USING THE MERN STACK,IT OFFERS REAL-TIME UPDATES THROUGH SOCKET.IO,  INCLUDES AUTHENTICATION AND AUTHORIZATION FEATURES, AND UTILIZES A DATABASE FOR STORING DATA",
 img:"https://raw.githubusercontent.com/haseebkhan9081/G-Keeper-App/master/public/GKA.png",
